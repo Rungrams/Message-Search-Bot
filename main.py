c1a681d2148cf5f3d2e0d8d5b622d850354f516d
Predator HackerzZ <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-07-25 09:16:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def inline_handlers(_, event: InlineQuery):
                     description="{}".format(message.text.rsplit("\n", 1)[-1]),
                     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝐒𝐞𝐚𝐫𝐜𝐡 𝐀𝐠𝐚𝐢𝐧", switch_inline_query_current_chat=""), InlineKeyboardButton("𝐆𝐨 𝐈𝐧𝐥𝐢𝐧𝐞", switch_inline_query="")]]),
                     input_message_content=InputTextMessageContent(
-                        message_text="{message.text.html}".format(message.text.rsplit("_", _)),
+                        message_text=message.text.markdown),
                         parse_mode="html",
                         disable_web_page_preview=True
                     )
